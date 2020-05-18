b9f45ccec1ee3d98344f23f871a0328c1e0878f5
Gain John <emperorduke5@gmail.com>
2020-05-18 13:55:22 +0100

    Added the front end

---
+++ resources/views/layouts/head.blade.php
@@ -0,0 +1,20 @@
+<meta charset="utf-8">
+<meta http-equiv="x-ua-compatible" content="ie=edge">
+<title>eCommerce HTML-5 Template </title>
+<meta name="description" content="">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="manifest" href="site.webmanifest">
+<link rel="shortcut icon" type="image/x-icon" href="assets/img/favicon.ico">
+
+<!-- CSS here -->
+    <link rel="stylesheet" href="{{asset("/css/bootstrap.min.css")}}">
+    <link rel="stylesheet" href="{{asset("/css/owl.carousel.min.css")}}">
+    <link rel="stylesheet" href="{{asset("/css/flaticon.css")}}">
+    <link rel="stylesheet" href="{{asset("css/slicknav.css")}}">
+    <link rel="stylesheet" href="{{asset("/css/animate.min.css")}}">
+    <link rel="stylesheet" href="{{asset("/css/magnific-popup.css")}}">
+    <link rel="stylesheet" href="{{asset("/css/fontawesome-all.min.css")}}">
+    <link rel="stylesheet" href="{{asset("/css/themify-icons.css")}}">
+    <link rel="stylesheet" href="{{asset("/css/slick.css")}}">
+    <link rel="stylesheet" href="{{asset("/css/nice-select.css")}}">
+    <link rel="stylesheet" href="{{asset("/css/style.css")}}">
